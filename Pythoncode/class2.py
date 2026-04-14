e2f425ac7fe1210ecf9320b58b1e83d4fcb95284
RaviKS <raviusa@yahoo.com>
2026-04-14 00:00:05 -0400

    python practice file

---
+++ Pythoncode/class2.py
@@ -0,0 +1,7 @@
+print("I am learning python")
+a = input("what is your name ?: ")
+b = input("what is your age ?: ")
+
+print("My name and my age is :", a, b)   
+print(f"My name is {a} and my age is {b}")
+print("My name is {} and my age is {}".format(a,b))
---
+++ Pythoncode/datatypes.py
@@ -0,0 +1,5 @@
+a = "Ravi Kumar" # String - any thing you put insite the double code its string onlyu]
+b = "123"
+c = "Im ravi"
+print (type(a))
+print (type(b))
